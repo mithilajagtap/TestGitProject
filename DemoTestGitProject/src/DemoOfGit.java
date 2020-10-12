f3f0c690238b4c72ac5bab53e40426a059ebb16a
mithilajagtap <72744759+mithilajagtap@users.noreply.github.com>
2020-10-12 18:47:11 +0530

    Update DemoOfGit.java
    Added second time SOP from browser

--- DemoTestGitProject/src/DemoOfGit.java
+++ DemoTestGitProject/src/DemoOfGit.java
@@ -3,6 +3,7 @@ public class DemoOfGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to Git Demo");
+		System.out.println("Hello Welcome to Git Demo second time from browser);
 	}
 
 }
